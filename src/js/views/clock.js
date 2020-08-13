8519bf9bd9f631d0ec1b6bca7a62820077194629
John Rohan <john.rohan87@gmail.com>
2020-08-13 03:07:09 +0000

    stuck on query selectior... updating clock

--- src/js/views/clock.js
+++ src/js/views/clock.js
@@ -4,9 +4,29 @@ import "../../styles/clock.scss";
 export const Clock = () => {
 	useEffect(() => {}, []);
 
+	const secondHand = document.querySelector(".second-hand");
+
+	function setDate() {
+		const now = new Date();
+		const seconds = now.getSeconds();
+		const secondsDegrees = (seconds / 60) * 360;
+		console.log(secondHand);
+		//secondHand.style.transform = "rotate(" + secondsDegrees + ")";
+		console.log(seconds);
+	}
+
+	setInterval(setDate, 1000);
+
 	return (
 		<div className="text-center mt-5 clockbg">
 			<h1 className="card">Clock</h1>
+			<div className="clock">
+				<div className="clock-face">
+					<div className="hand hour-hand" />
+					<div className="hand min-hand" />
+					<div className="hand second-hand" />
+				</div>
+			</div>
 		</div>
 	);
 };
