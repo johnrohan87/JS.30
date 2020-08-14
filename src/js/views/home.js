edb37f15a229299287e78d6779adf17606a6688c
John Rohan <john.rohan87@gmail.com>
2020-08-14 03:49:40 +0000

    added homepage image

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,12 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import JS30 from "../../img/JS30.png";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="h2 text-center mt-5 bg-primary card">Java Script 30 Day Challenge!</div>
+		<div className="container-fluid">
+			<img src={JS30} />
+		</div>
 	</div>
 );
