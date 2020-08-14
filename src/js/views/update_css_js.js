2dab0509bb8429df7c3a04ca6906ad391d71bf1f
John Rohan <john.rohan87@gmail.com>
2020-08-14 02:16:47 +0000

    set scss for page. finished setup.

--- src/js/views/update_css_js.js
+++ src/js/views/update_css_js.js
@@ -5,10 +5,26 @@ export const UpdateCssJs = () => {
 	useEffect(() => {}, []);
 
 	return (
-		<div>
-			<h1 className="text-center mt-5 bg-primary card">Update CSS Vairables with JS</h1>
+		<div className="updateBG">
+			<h2 className="text-center mt-5 bg-primary card">
+				Update CSS Variables with <span className="hl">JS</span>
+			</h2>
 
-			<div className="text-center updateBG">Hello</div>
+			<div className="text-center">
+				<div className="controls">
+					<label htmlFor="spacing">Spacing:</label>
+					<input id="spacing" type="range" name="spacing" min="10" max="200" value="10" data-sizing="px" />
+
+					<label htmlFor="blur">Blur:</label>
+					<input id="blur" type="range" name="blur" min="0" max="25" value="10" data-sizing="px" />
+
+					<label htmlFor="base">Base Color</label>
+
+					<input id="base" type="color" name="base" value="#ffc600" />
+				</div>
+
+				<img src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
+			</div>
 		</div>
 	);
 };
