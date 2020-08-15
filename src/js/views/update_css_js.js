9df9dccfb4f7fbf157ddbdc9acd37c28751d3de6
John Rohan <john.rohan87@gmail.com>
2020-08-15 00:12:06 +0000

    finished hooks for day 3 all working

--- src/js/views/update_css_js.js
+++ src/js/views/update_css_js.js
@@ -3,6 +3,9 @@ import "../../styles/update_css_js.scss";
 
 export const UpdateCssJs = () => {
 	useEffect(() => {}, []);
+	const [imgSpacing, setImgSpacing] = useState("10");
+	const [imgBlur, setImgBlur] = useState("10");
+	const [imgColor, setImgColor] = useState("#ffc600");
 
 	return (
 		<div className="updateBG">
@@ -13,17 +16,58 @@ export const UpdateCssJs = () => {
 			<div className="text-center">
 				<div className="controls m-2">
 					<label htmlFor="spacing">Spacing:</label>
-					<input id="spacing" type="range" name="spacing" min="10" max="200" value="10" data-sizing="px" />
+					<input
+						id="spacing"
+						type="range"
+						name="spacing"
+						min="10"
+						max="200"
+						value={imgSpacing}
+						data-sizing="px"
+						onChange={e => {
+							setImgSpacing(e.target.value);
+							//console.log(imgSpacing);
+						}}
+					/>
 
 					<label htmlFor="blur">Blur:</label>
-					<input id="blur" type="range" name="blur" min="0" max="25" value="10" data-sizing="px" />
+					<input
+						id="blur"
+						type="range"
+						name="blur"
+						min="0"
+						max="25"
+						value={imgBlur}
+						data-sizing="px"
+						onChange={e => {
+							setImgBlur(e.target.value);
+							//console.log(imgSpacing);
+						}}
+					/>
 
 					<label htmlFor="base">Base Color</label>
 
-					<input id="base" type="color" name="base" value="#ffc600" />
+					<input
+						id="base"
+						type="color"
+						name="base"
+						value={imgColor}
+						onChange={e => {
+							setImgColor(e.target.value);
+							//console.log(imgSpacing);
+						}}
+					/>
 				</div>
 
-				<img className="img-fluid" src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
+				<img
+					style={{
+						padding: imgSpacing + "px",
+						background: imgColor,
+						filter: "blur(" + imgBlur + "px)"
+					}}
+					className="img-fluid"
+					src="https://source.unsplash.com/7bwQXzbF6KE/800x500"
+				/>
 			</div>
 		</div>
 	);
