d88ea666b0a92856fff268d6c582e37ab4a53fd4
John Rohan <john.rohan87@gmail.com>
2020-11-13 00:06:15 +0000

    updating touch settings canvas

--- src/js/layout.js
+++ src/js/layout.js
@@ -24,7 +24,7 @@ const Layout = () => {
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div className="d-flex flex-column h-100">
+		<div className="d-flex flex-column">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
