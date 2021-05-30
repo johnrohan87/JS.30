13f8072c8c1e750bef011dccd1aee0cccf1d872f
John Rohan <john.rohan87@gmail.com>
2021-05-30 13:14:30 -0400

    checking for bugs

--- src/js/views/webcam_fun.js
+++ src/js/views/webcam_fun.js
@@ -12,16 +12,26 @@ export const WebcamFun = () => {
 	let ctx = null;
 	const [photos, setPhotos] = useState({ album: [] });
 	const [photoText, setPhotoText] = useState("");
+	const [redMin, setRedMin] = useState("128");
+	const [redMax, setRedMax] = useState("128");
+	const [greenMin, setGreenMin] = useState("128");
+	const [greenMax, setGreenMax] = useState("128");
+	const [blueMin, setBlueMin] = useState("128");
+	const [blueMax, setBlueMax] = useState("128");
 
-	useEffect(() => {
-		// eslint-disable-line no-console
-		video = document.querySelector(".player");
-		//canvas = document.querySelector(".photo");
-		ctx = canvas.current.getContext("2d");
-		//strip = document.querySelector(".strip");
-		//snap = document.querySelector(".snap");
-		getVideo();
-	}, []);
+	useEffect(
+		() => {
+			// eslint-disable-line no-console
+			video = document.querySelector(".player");
+			//canvas = document.querySelector(".photo");
+			ctx = canvas.current.getContext("2d");
+			//strip = document.querySelector(".strip");
+			//snap = document.querySelector(".snap");
+			getVideo();
+			//console.log(redMin, redMax, greenMin, greenMax, blueMin, blueMax);
+		},
+		[redMin, redMax, greenMin, greenMax, blueMin, blueMax]
+	);
 
 	function getVideo() {
 		navigator.mediaDevices
@@ -45,9 +55,13 @@ export const WebcamFun = () => {
 		return setInterval(() => {
 			ctx.drawImage(video, 0, 0, width, height);
 			let pixels = ctx.getImageData(0, 0, width, height);
+
 			//pixels = redEffect(pixels);
-			pixels = rgbSplit(pixels);
-			ctx.globalAlpha = 0.1;
+			//pixels = rgbSplit(pixels);
+			//ctx.globalAlpha = 0.1;
+
+			pixels = greenScreen(pixels);
+
 			ctx.putImageData(pixels, 0, 0);
 		}, 16);
 	}
@@ -77,23 +91,47 @@ export const WebcamFun = () => {
 	function redEffect(pixels) {
 		for (let i = 0; i < pixels.data.length; i += 4) {
 			pixels.data[i] = pixels.data[i] + 100; //red
-			pixels.data[i + 1] = pixels.data[i + 1] - 50; //red
-			pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //red
+			pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+			pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
 		}
 		return pixels;
 	}
 	function rgbSplit(pixels) {
 		for (let i = 0; i < pixels.data.length; i += 4) {
 			pixels.data[i - 150] = pixels.data[i] + 100; //red
-			pixels.data[i + 100] = pixels.data[i + 1] - 50; //red
-			pixels.data[i - 150] = pixels.data[i + 2] * 0.5; //red
+			pixels.data[i + 100] = pixels.data[i + 1] - 50; //green
+			pixels.data[i - 150] = pixels.data[i + 2] * 0.5; //blue
 		}
 		return pixels;
 	}
 	function greenScreen(pixels) {
-		const levels = {};
+		//const levels = {};
 
-		//[...doc]
+		//console.log(pixels.data);
+		//debugger;
+		//console.log(redMin, redMax, greenMin, greenMax, blueMin, blueMax);
+		for (let i = 0; i < pixels.data.length; i = i + 4) {
+			let red = pixels.data[i]; //red
+			let green = pixels.data[i + 1]; //green
+			let blue = pixels.data[i + 2]; //blue
+			let alpha = pixels.data[i + 3]; //alpha
+
+			//console.log(pixels.data[i], pixels.data[i + 1], pixels.data[i + 2], pixels.data[i + 3]);
+
+			if (
+				red >= redMin &&
+				green >= greenMin &&
+				blue >= blueMin &&
+				red <= redMax &&
+				green <= greenMax &&
+				blue <= blueMax
+			) {
+				// remove the alpha
+				pixels.data[i + 3] = 0;
+				//console.log(red, green, blue, alpha);
+			}
+		}
+		return pixels;
 	}
 
 	return (
@@ -105,20 +143,82 @@ export const WebcamFun = () => {
 					<button onClick={() => takePhoto()}>Take Photo</button>
 
 					<div className="rgb">
-						<label htmlFor="rmin">Red Min:</label>
-						<input type="range" min="0" max="255" name="rmin" />
-						<label htmlFor="rmax">Red Max:</label>
-						<input type="range" min="0" max="255" name="rmax" />
+						<label htmlFor="rmin">
+							Red Min:
+							{redMin}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="rmin"
+							value={redMin}
+							onChange={e => {
+								setRedMin(e.target.value);
+							}}
+						/>
+						<label htmlFor="rmax">
+							Red Max:
+							{redMax}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="rmax"
+							value={redMax}
+							onChange={e => setRedMax(e.target.value)}
+						/>
 						<br />
-						<label htmlFor="gmin">Green Min:</label>
-						<input type="range" min="0" max="255" name="gmin" />
-						<label htmlFor="gmax">Green Max:</label>
-						<input type="range" min="0" max="255" name="gmax" />
+						<label htmlFor="gmin">
+							Green Min:
+							{greenMin}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="gmin"
+							value={greenMin}
+							onChange={e => setGreenMin(e.target.value)}
+						/>
+						<label htmlFor="gmax">
+							Green Max:
+							{greenMax}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="gmax"
+							value={greenMax}
+							onChange={e => setGreenMax(e.target.value)}
+						/>
 						<br />
-						<label htmlFor="bmin">Blue Min:</label>
-						<input type="range" min="0" max="255" name="bmin" />
-						<label htmlFor="bmax">Blue Max:</label>
-						<input type="range" min="0" max="255" name="bmax" />
+						<label htmlFor="bmin">
+							Blue Min:
+							{blueMin}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="bmin"
+							value={blueMin}
+							onChange={e => setBlueMin(e.target.value)}
+						/>
+						<label htmlFor="bmax">
+							Blue Max:
+							{blueMax}
+						</label>
+						<input
+							type="range"
+							min="0"
+							max="255"
+							name="bmax"
+							value={blueMax}
+							onChange={e => setBlueMax(e.target.value)}
+						/>
 					</div>
 				</div>
 
